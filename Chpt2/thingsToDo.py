666f7350950b8fc3fc533fa8a2b67d412e397368
Patrick Shaw <sixtey7@gmail.com>
2019-08-19 20:02:08 -0400

    complete the things to do from chapter 2

---
+++ Chpt2/thingsToDo.py
@@ -0,0 +1,18 @@
+# Thing 1 and 2
+secondsInMinute = 60
+minutesInHour = 60
+secondsInHour = secondsInMinute * minutesInHour
+
+print("Number of seconds in an hour:", secondsInHour)
+
+# Thing 3 and 4
+hoursPerDay = 24
+secondsInDay = secondsInHour * hoursPerDay
+
+print("Number of seconds in a day:", secondsInDay)
+
+# Thing 5
+print("Thing 5 answer:", secondsInDay / secondsInHour)
+
+#Thing 6
+print("Thing 5 answer:", secondsInDay // secondsInHour)
