5e82938395f4ba11a93d1265f4f88ad120a1f2a8
Patrick Shaw <sixtey7@gmail.com>
2019-09-14 10:16:49 -0400

    installed pycharm, linter ran wild

--- flask/todo.py
+++ flask/todo.py
@@ -1,10 +1,10 @@
-class Todo():
+class Todo:
     def __init__(self, id, title):
         self.id = id
         self.title = title
         self.completed = False
 
-    #TODO: This feel like a hack, I shouldn't need to make this method if I knew what I was doing
+    # TODO: This feel like a hack, I shouldn't need to make this method if I knew what I was doing
     def toObj(self):
         return {
             'id': self.id,
--- flask/todoDB.py
+++ flask/todoDB.py
@@ -1,10 +1,11 @@
-#import sqlite3
+# import sqlite3
 from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 
-class TodoDB():
+
+class TodoDB:
     def __init__(self):
-        print ('Creating the database!')
+        print('Creating the database!')
 
         self.engine = create_engine('sqlite:///todos.db', echo=True)
 
@@ -12,8 +13,6 @@ class TodoDB():
 
         class Todo(Base):
             __tablename__ = 'todo'
-            id = Column(Integer, primary_key = True)
+            id = Column(Integer, primary_key=True)
             title = Column(String)
             completed = Column(String)
-
-        
