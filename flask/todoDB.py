1839481e34b73925a2892d090738ac0fb4bb403c
Patrick Shaw <sixtey7@gmail.com>
2019-09-13 20:27:31 -0400

    start working on getting a db together

---
+++ flask/todoDB.py
@@ -0,0 +1,19 @@
+#import sqlite3
+from sqlalchemy import *
+from sqlalchemy.ext.declarative import declarative_base
+
+class TodoDB():
+    def __init__(self):
+        print ('Creating the database!')
+
+        self.engine = create_engine('sqlite:///todos.db', echo=True)
+
+        Base = declarative_base()
+
+        class Todo(Base):
+            __tablename__ = 'todo'
+            id = Column(Integer, primary_key = True)
+            title = Column(String)
+            completed = Column(String)
+
+        
