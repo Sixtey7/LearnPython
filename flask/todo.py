c9b2df6815356b4f11761d6cd2f04e2aa15a0580
Patrick Shaw <sixtey7@gmail.com>
2019-09-15 08:06:46 -0400

    python hates capitals

--- flask/todo.py
+++ flask/todo.py
@@ -1,11 +1,11 @@
 class Todo:
-    def __init__(self, id, title):
-        self.id = id
+    def __init__(self, todo_id, title):
+        self.id = todo_id
         self.title = title
         self.completed = False
 
     # TODO: This feel like a hack, I shouldn't need to make this method if I knew what I was doing
-    def toObj(self):
+    def to_obj(self):
         return {
             'id': self.id,
             'title': self.title,
--- flask/todos.py
+++ flask/todos.py
@@ -6,42 +6,47 @@ todos = {}
 
 app = Flask(__name__)
 
+
 @app.route('/todos/healthz')
 def healthz():
     return "All's Well"
 
+
 @app.route('/todos', methods=['GET'])
-def getAll():
-    #TODO: This is gnarly, I need to look into a better way to do this
-    return jsonify([todos[todoId].toObj() for todoId in todos]), 200
+def get_all():
+    # TODO: This is gnarly, I need to look into a better way to do this
+    return jsonify([todos[todoId].to_obj() for todoId in todos]), 200
+
 
 @app.route('/todos/<string:todo_id>', methods=['GET'])
-def getRoute(todo_id):
+def get_route(todo_id):
     print('Got the id %s' % todo_id)
 
-    return jsonify(todos[todo_id].toObj())
+    return jsonify(todos[todo_id].to_obj())
+
 
 @app.route('/todos', methods=['POST'])
-def createObj():
+def create_obj():
     if not request.json:
         abort(400, 'No request body provided!')
 
     if 'id' in request.json:
-        todoId = request.json['id']
+        todo_id = request.json['id']
     else:
-        todoId = str(uuid4())
+        todo_id = str(uuid4())
 
-    todo = Todo(todoId, request.json['title'])
+    todo = Todo(todo_id, request.json['title'])
 
-    todos[todoId] = todo
+    todos[todo_id] = todo
 
-    return jsonify({'todo': todo.toObj()}), 201
+    return jsonify({'todo': todo.to_obj()}), 201
+
 
 @app.route('/todos/<string:todo_id>', methods=['PUT'])
-def updateObj(todo_id):
+def update_obj(todo_id):
     if not request.json:
         abort(400, 'No request body provided!')
-    
+
     if not todos[todo_id]:
         abort(400, 'Id does not exist, use POST to create new todo')
 
@@ -50,22 +55,25 @@ def updateObj(todo_id):
 
     return jsonify({'todo': todo}), 201
 
+
 @app.route('/todos/<string:todo_id>/<string:complete>', methods=['PUT'])
-def toggleComplete(todo_id, complete):
+def toggle_complete(todo_id, complete):
     if not todos[todo_id]:
         abort(400, 'Id does not exist, use POST to create new todo')
 
     todos[todo_id]['complete'] = bool(complete)
 
-    return jsonify({'todo': todos[todo_id].toObj()}), 201
+    return jsonify({'todo': todos[todo_id].to_obj()}), 201
+
 
 @app.route('/todos/<string:todo_id>', methods=['DELETE'])
-def deleteTodo(todo_id):
+def delete_todo(todo_id):
     if not todos[todo_id]:
-        abort(400, 'Cannot delete an id that doesn\'t exist')
-    
+        abort(400, "Cannot delete an id that doesn't exist")
+
     del todos[todo_id]
 
     return '', 200
+
 
 app.run(port=5000, debug=True)
